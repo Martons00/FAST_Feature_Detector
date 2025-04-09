8f05e263b0046ddda14051e27931a9d57ae448f4
Martons00 <raffaelemartone34@gmail.com>
2025-04-09 13:04:38 +0200

    Update Assegnament.py

--- Assegnament.py
+++ Assegnament.py
@@ -57,16 +57,14 @@ while cap.isOpened():
     img_h, img_w, img_c = image.shape
 
     # Left eye indices list
-    #LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
+    LEFT_POS_INT =[ 133,158,160,33,144,153]
     # Right eye indices list
-    #RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]
-    #LEFT_IRIS = [473, 474, 475, 476, 477]
-    #RIGHT_IRIS = [468, 469, 470, 471, 472]
-    LEFT_POS_INT = [386, 374]
-    RIGHT_POS_INT = [159,145]
+    RIGHT_POS_INT=[ 362,385,387,263,373,380]
 
-    left_eye_pos = []
-    right_eye_pos = []
+    left_eye_pos_2d = []
+    right_eye_pos_2d = []
+    
+
     closed = False
     responsive = False
 
@@ -74,59 +72,47 @@ while cap.isOpened():
         for face_landmarks in results.multi_face_landmarks:
             for idx, lm in enumerate(face_landmarks.landmark):
 
-                if idx in LEFT_POS_INT:
+                if idx == LEFT_POS_INT[0]:
+                    left_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 0])
                     cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 0, 255), thickness=-1)
-                    left_eye_pos.append( int(lm.y * img_h))
+                if idx == LEFT_POS_INT[1]:
+                    left_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 1])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 0, 255), thickness=-1)
+                if idx == LEFT_POS_INT[2]:  
+                    left_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 2])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 0, 255), thickness=-1)
+                if idx == LEFT_POS_INT[3]:
+                    left_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 3])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 0, 255), thickness=-1)
+                if idx == LEFT_POS_INT[4]:
+                    left_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 4])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 0, 255), thickness=-1)
+                if idx == LEFT_POS_INT[5]:
+                    left_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 5])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 0, 255), thickness=-1)
                 
-
-                if idx in RIGHT_POS_INT:
+                if idx == RIGHT_POS_INT[0]:
+                    right_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 0])
                     cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 255, 0), thickness=-1)
-                    right_eye_pos.append( int(lm.y * img_h))
-                
-                if idx == 4:
-                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(255, 0, 0), thickness=-1)
-                    responsive = True
+                if idx == RIGHT_POS_INT[1]:         
+                    right_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 1])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 255, 0), thickness=-1)
+                if idx == RIGHT_POS_INT[2]:
+                    right_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 2])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 255, 0), thickness=-1)
+                if idx == RIGHT_POS_INT[3]:
+                    right_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 3])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 255, 0), thickness=-1)
+                if idx == RIGHT_POS_INT[4]:
+                    right_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 4])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 255, 0), thickness=-1)
+                if idx == RIGHT_POS_INT[5]:
+                    right_eye_pos_2d.append([int(lm.x * img_w), int(lm.y * img_h), 5])
+                    cv2.circle(image, (int(lm.x * img_w), int(lm.y * img_h)), radius=5, color=(0, 255, 0), thickness=-1)
+    
+    
 		
 
-        #condizione di chiusura occhio
-        if left_eye_pos and right_eye_pos:
-            left_eye = np.array(left_eye_pos)
-            right_eye = np.array(right_eye_pos)
-
-            distance_left = np.linalg.norm(left_eye[0] - left_eye[1])
-            distance_right = np.linalg.norm(right_eye[0] - right_eye[1])
-            cv2.putText(image, "Distance left: " + str(distance_left), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            cv2.putText(image, "Distance right: " + str(distance_right), (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-
-            if distance_left < 15 and distance_right < 15:
-                closed = True
-                currentWindowS = time.time() - start
-            elif currentWindowS > 0 :
-                closed = False
-                currentWindowS = 0
-                end = time.time()
-                totalTime = end-start
-
-       
-
-        
-        cv2.putText(image, "Current Window: " + str(currentWindowS), (50, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.putText(image, "Last Total Time: " + str(totalTime), (50, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.putText(image, "Current Window Unresponsive Driver: " + str(currentWindowU), (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        if currentWindowS > thresholdMSleepy and currentWindowS < thresholdSleepy:  
-            cv2.putText(image, "MicroSleepy", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        elif currentWindowS > thresholdSleepy:  
-            cv2.putText(image, "Sleepy", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        elif currentWindowS > 6:
-            cv2.putText(image, "Unresponsive Driver", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    if responsive == False:
-            currentWindowU = time.time() - startU
-    else:
-            currentWindowU = 0
-    if currentWindowU > thresholdUnresponsive:
-        cv2.putText(image, "Unresponsive Driver", (200, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     cv2.imshow('output window', image)       
 
